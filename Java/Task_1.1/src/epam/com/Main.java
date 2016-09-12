6948ba5ac96bf0890c86f8caf24feb703e63c743
Ruslan <Ruslan.Zaporozhenko@gmail.com>
2016-09-12 08:48:39 +0300

    Task_1.1

---
+++ Java/Task_1.1/src/epam/com/Main.java
@@ -0,0 +1,17 @@
+package epam.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 12;
+        int b = 4;
+        int c = 3;
+        int d = 158;
+        int e = 2;
+
+        int f = (a*b/c + d/e);
+
+        System.out.println(f);
+    }
+}
